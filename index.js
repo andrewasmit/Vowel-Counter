57856154c741b0a0d2bf589cf2a70d1abdef1289
andrew <andrewasmit@hotmail.com>
2022-10-08 13:04:54 -0500

    Small updates to function

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ inputBtn.addEventListener("click", countVowels)
 
 // Setting the Enter button as a button click submit
 const userString = document.querySelector('#stringUserInput')
-userString.addEventListener("keypress", function(event){
+userString.addEventListener("keydown", function(event){
     if (event.key==="Enter"){
         event.preventDefault();
         document.getElementById('inputBtn').click();
     }
-})
+});
+
 
 // Defining vowels and counting vowels functions
 const vowels = ['a', 'e', 'i', 'o', 'u']
@@ -22,18 +23,23 @@ for (let i =0; i<str.length; i++){
 if (vowels.includes(str[i])){
     counter++;
     vowelArray.push(str[i]);
+    userString.value="";
     }
     if (!vowels.includes(str[i]) && str[i]==='y'){
         console.log(`${str[i]} is sometimes a vowel.`)
         counter++;
         vowelArray.push(str[i]);
+        userString.value="";
     }
 }
 if (counter ===1){
 console.log(`This string has ${counter} vowel, which is ${vowelArray}.`);
+userString.value="";
 } if (counter ===0){
     console.log("This string has no vowels.");
+    userString.value="";
 } if (counter>1) {console.log(`This string has ${counter} vowels, which are ${vowelArray}.`);
+userString.value="";
 }};
 
 
